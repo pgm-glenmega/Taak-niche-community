10c35342d93fd44bb93886df2b363091a9c7c4f5
pgm-glenmega <glenmega@student.arteveldehs.be>
2024-01-20 12:18:35 +0100

    fix filter exercises

--- resources/views/exercises/partials/filterexercises.blade.php
+++ resources/views/exercises/partials/filterexercises.blade.php
@@ -1,5 +1,5 @@
 <div class="flex my-8">
-    <form action="{{ route('workouts.main') }}" method="get" class="flex-1">
+    <form action="{{ route('exercises.main') }}" method="get" class="flex-1">
         <button type="submit" name="body_part" value="{{ null }}" class="mr-2 flex-1 {{ is_null($selectedBodyPart) ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700' }} px-4 py-2 rounded-md">All Body Parts</button>
         <button type="submit" name="body_part" value="legs" class="mr-2 flex-1 {{ $selectedBodyPart === 'legs' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700' }} px-4 py-2 rounded-md">Legs</button>
         <button type="submit" name="body_part" value="arms" class="mr-2 flex-1 {{ $selectedBodyPart === 'arms' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700' }} px-4 py-2 rounded-md">Arms</button>
